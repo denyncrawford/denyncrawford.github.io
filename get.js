b98806d6b090a2540b9d24e368005b6b4f4de6c2
Denyn Crawford <kiquerangelb@gmail.com>
2018-09-06 19:17:56 -0400

    Add existing file

---
+++ get.js
@@ -0,0 +1,6 @@
+//https://9ooovpsfe5wdckz27ccwza-on.drv.tw/CDN/
+
+
+$("body").load("https://denyncrawford.github.io/index.html #chat");
+
+$("head").html('<link rel="stylesheet" href="https://denyncrawford.github.io/main.css">');
